58bb52dc3be493c4098612181a0af7e2d2c604da
AshtonVang001 <radturkdude@gmail.com>
2026-02-03 10:32:04 -0800

    add: add enemies class

---
+++ 2DGame/include/_enemies.h
@@ -0,0 +1,16 @@
+#ifndef _ENEMIES_H
+#define _ENEMIES_H
+
+
+class _enemies
+{
+    public:
+        _enemies();
+        virtual ~_enemies();
+
+    protected:
+
+    private:
+};
+
+#endif // _ENEMIES_H
---
+++ 2DGame/src/_enemies.cpp
@@ -0,0 +1,11 @@
+#include "_enemies.h"
+
+_enemies::_enemies()
+{
+    //ctor
+}
+
+_enemies::~_enemies()
+{
+    //dtor
+}
